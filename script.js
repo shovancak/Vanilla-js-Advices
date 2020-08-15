dcdea9c74d39326b8cf7fc9c5b029a594e8e3d4f
shovancak <s.hovancak@gmail.com>
2020-08-15 14:42:33 +0200

    Creating function for fetching advices from API

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-console.log("working");
-alert("WORKING");
+const getAdvice = async () => {
+  const apiUrl = "https://api.adviceslip.com/advice";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const advice = data.slip.advice;
+    console.log(advice);
+  } catch (error) {
+    console.log("Something went wrong. Error: ", error);
+  }
+};
+
+getAdvice();
