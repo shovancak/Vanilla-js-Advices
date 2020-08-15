2a7c797a941ce0ed2595353d9f9199d10e23b835
shovancak <s.hovancak@gmail.com>
2020-08-15 15:35:02 +0200

    Populating html elements with data from API

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+const containerElement = document.getElementById("container");
+const adviceElement = document.getElementById("advice");
+const authorElement = document.getElementById("author");
+const twitterElement = document.getElementById("twitter");
+const newAdviceElement = document.getElementById("new-advice");
+
 const getAdvice = async () => {
   const adviceApiUrl = "https://api.adviceslip.com/advice";
   try {
     const response = await fetch(adviceApiUrl);
     const data = await response.json();
     const advice = data.slip.advice;
-    console.log(advice);
+    adviceElement.innerText = advice;
+    if (advice.length > 125) {
+      adviceElement.classList.add("long-advice");
+    } else {
+      adviceElement.classList.remove("long-advice");
+    }
   } catch (error) {
     console.log("Something went wrong. Error: ", error);
   }
@@ -16,7 +27,7 @@ const getAuthor = async () => {
     const response = await fetch(authorApiUrl);
     const data = await response.json();
     const author = data.results[0].name.first;
-    console.log(author);
+    authorElement.innerText = author;
   } catch (error) {
     console.log("Something went wrong. Error: ", error);
   }
